49cff4ea0b336541103fa531a79e7b1792089286
venerate0112 <veneratemdakahlonipho@gmail.com>
2026-02-02 14:46:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -453,6 +453,15 @@ elif menu == "References":
 
     """)
 
+# --- FOOTER ---
+st.markdown("---")
+footer_col1, footer_col2, footer_col3 = st.columns(3)
+with footer_col1:
+    st.caption("© 2024 Venerate Mdaka")
+with footer_col2:
+    st.caption("University of Mpumalanga")
+with footer_col3:
+    st.caption("BSc Honours Research Project")
 
 
 
